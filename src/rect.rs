4157c19146bea78fc38e8cd76f32b388403d84cd
Felix Schütt <felix.schuett@maps4print.com>
2017-06-25 10:02:37 +0200

    Improved memory layout

--- src/rect.rs
+++ src/rect.rs
@@ -6,24 +6,16 @@ extern crate glium;
 use renderer::Vertex;
 use test::Bencher;
 
-#[derive(Debug)]
-pub struct Point2 {
-    x: f32,
-    y: f32,
-}
-
 /// A finite rectangle in pixel coordinates that will end up on the screen
 #[derive(Debug)]
 pub struct Rect {
     z_index: f32,
-    /// Top left corner
-    tl: Point2,
-    /// Top right corner
-    tr: Point2,
-    /// Bottom left corner
-    bl: Point2,
-    /// Bottom right corner
-    br: Point2,
+    /// x coordinates - as an array because of simd layout
+    /// tl, tr, bl, br 
+    x: [f32; 4],
+    /// y coordinates - as an array because of simd layout
+    /// top left, top right, bottom left, bottom right 
+    y: [f32; 4],
 }
 
 impl Rect {
@@ -32,14 +24,13 @@ impl Rect {
     -> Self
     {
         Self {
-            tl: Point2 { x: left, y: top },
-            tr: Point2 { x: right, y: top },
-            bl: Point2 { x: left, y: bottom },
-            br: Point2 { x: right, y: bottom },
+            x: [left, right, left, right],
+            y: [top, top, bottom, bottom],
             z_index: z,
         }
     }
 
+/*
     pub fn rotate_center_no_simd(&mut self, in_angle: f32)
     {
         let center_y = ((self.tr.y - self.bl.y) * 0.5) + self.bl.y;
@@ -69,15 +60,15 @@ impl Rect {
         self.tl.x += center_x; self.tr.x += center_x; self.bl.x += center_x; self.br.x += center_x;
         self.tl.y += center_y; self.tr.y += center_y; self.bl.y += center_y; self.br.y += center_y;
     }
-
+*/
     // rotates the rectangle around its center
     pub fn rotate_center(&mut self, in_angle: f32)
     {
-        let center_y = ((self.tr.y - self.bl.y) * 0.5) + self.bl.y;
-        let center_x = ((self.tr.x - self.bl.x) * 0.5) + self.bl.x;
+        let center_y = ((self.y[0] - self.y[2]) * 0.5) + self.y[2];
+        let center_x = ((self.x[1] - self.x[0]) * 0.5) + self.x[0];
 
-        let mut simd_x_dir = simd::f32x4::new(self.tl.x, self.tr.x, self.bl.x, self.br.x);
-        let mut simd_y_dir = simd::f32x4::new(self.tl.y, self.tr.y, self.bl.y, self.br.y);
+        let mut simd_x_dir = simd::f32x4::load(&self.x, 0);
+        let mut simd_y_dir = simd::f32x4::load(&self.y, 0);
 
         // move all points to origin
         simd_x_dir = simd_x_dir - simd::f32x4::splat(center_x);
@@ -94,24 +85,26 @@ impl Rect {
         simd_x_new = simd_x_new + simd::f32x4::splat(center_x);
         simd_y_dir = simd_y_dir + simd::f32x4::splat(center_y);
 
-        self.tl.x = simd_x_new.extract(0); self.tr.x = simd_x_new.extract(1); self.bl.x = simd_x_new.extract(2); self.br.x = simd_x_new.extract(3);
-        self.tl.y = simd_y_dir.extract(0); self.tr.y = simd_y_dir.extract(1); self.bl.y = simd_y_dir.extract(2); self.br.y = simd_y_dir.extract(3);
+        simd_x_new.store(&mut self.x, 0);
+        simd_y_dir.store(&mut self.y, 0);
     }
 
+/*
     pub fn translate_no_simd(&mut self, x: f32, y: f32)
     {
         self.tr.x += x; self.tl.x += x; self.br.x += x; self.bl.x += x;
         self.tr.y += y; self.tl.y += y; self.br.y += x; self.bl.y += y;
     }
+*/
 
     // translates a rectangle
     pub fn translate(&mut self, x: f32, y: f32)
     {
-        let simd_x_dir = simd::f32x4::new(self.tl.x, self.tr.x, self.bl.x, self.br.x) + simd::f32x4::splat(x);
-        let simd_y_dir = simd::f32x4::new(self.tl.y, self.tr.y, self.bl.y, self.br.y) + simd::f32x4::splat(y);
+        let simd_x_dir = simd::f32x4::load(&self.x, 0) + simd::f32x4::splat(x);
+        let simd_y_dir = simd::f32x4::load(&self.y, 0) + simd::f32x4::splat(y);
 
-        self.tl.x = simd_x_dir.extract(0); self.tr.x = simd_x_dir.extract(1); self.bl.x = simd_x_dir.extract(2); self.br.x = simd_x_dir.extract(3);
-        self.tl.y = simd_y_dir.extract(0); self.tr.y = simd_y_dir.extract(1); self.bl.y = simd_y_dir.extract(2); self.br.y = simd_y_dir.extract(3);
+        simd_x_dir.store(&mut self.x, 0);
+        simd_y_dir.store(&mut self.y, 0);
     }
 }
 
@@ -120,13 +113,13 @@ impl ::std::convert::Into<Vec<Vertex>> for Rect {
     -> Vec<Vertex>
     {
         return vec![
-            Vertex { position: [self.tl.x,    self.tl.y, self.z_index] }, /*top left*/
-            Vertex { position: [self.bl.x,    self.bl.y, self.z_index] }, /*bottom left*/
-            Vertex { position: [self.tr.x,    self.tr.y, self.z_index] }, /*top right*/
+            Vertex { position: [self.x[0],    self.y[0], self.z_index] }, /*top left*/
+            Vertex { position: [self.x[2],    self.y[2], self.z_index] }, /*bottom left*/
+            Vertex { position: [self.x[1],    self.y[1], self.z_index] }, /*top right*/
             
-            Vertex { position: [self.br.x,    self.br.y, self.z_index] }, /*bottom right*/
-            Vertex { position: [self.tr.x,    self.tr.y, self.z_index] }, /*top right*/
-            Vertex { position: [self.bl.x,    self.bl.y, self.z_index] }, /*bottom left*/
+            Vertex { position: [self.x[3],    self.y[3], self.z_index] }, /*bottom right*/
+            Vertex { position: [self.x[1],    self.y[1], self.z_index] }, /*top right*/
+            Vertex { position: [self.x[2],    self.y[2], self.z_index] }, /*bottom left*/
         ];
     }
 }
@@ -147,6 +140,7 @@ fn bench_rotate_center(b: &mut Bencher) {
     })
 }
 
+/*
 #[bench]
 fn bench_rotate_center_no_simd(b: &mut Bencher) {
     let mut rand_angles = Vec::new();
@@ -162,6 +156,7 @@ fn bench_rotate_center_no_simd(b: &mut Bencher) {
         }
     })
 }
+*/
 
 #[bench]
 fn bench_translate(b: &mut Bencher) {
@@ -179,6 +174,7 @@ fn bench_translate(b: &mut Bencher) {
     })
 }
 
+/*
 #[bench]
 fn bench_translate_no_simd(b: &mut Bencher) {
     let mut rand_angles = Vec::new();
@@ -194,3 +190,4 @@ fn bench_translate_no_simd(b: &mut Bencher) {
         }
     })
 }
+*/
